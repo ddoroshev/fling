1ab15cf9d24a68559964f1825162ed3572712b09
Dmitrii Doroshev <github@doroshev.com>
2025-08-16 18:20:51 +0200

    Fix the version script

--- get_version.sh
+++ get_version.sh
@@ -5,6 +5,8 @@ GIT_DESC=$(git describe --tags --always --dirty 2>/dev/null)
 
 if [[ "$GIT_DESC" == "v$VERSION" ]]; then
     echo "$VERSION"
+elif [[ "$GIT_DESC" == "v$VERSION-dirty" ]]; then
+    echo "$VERSION-dirty"
 else
     echo "$VERSION-$GIT_DESC"
 fi
